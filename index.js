198a2ecde7e21d3aaa08a3e04ec6be0905b29e26
Soe Nay Lin <soenaylin.g@gmail.com>
2025-12-03 14:33:57 +0630

    before remove nginx and change project structure

--- index.js
+++ index.js
@@ -12,19 +12,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const { usersRouter } = require("./routers/users");
-app.use('/users', usersRouter);
+app.use("/users", usersRouter);
 
 const { postsRouter } = require("./routers/posts");
-app.use('/posts', postsRouter);
+app.use("/posts", postsRouter);
 
 const { notiRouter } = require("./routers/notis");
-app.use('/notis', notiRouter);
+app.use("/notis", notiRouter);
 
 const { wsRouter } = require("./routers/ws");
 app.use(wsRouter);
 
 app.use("/images", express.static(process.env.IMAGES_PATH));
 
-app.listen(process.env.PORT, () => {
-    console.log(`X Api running at ${process.env.PORT}...`);
+// app.listen(process.env.PORT, "0.0.0.0", () => {
+// 	console.log(`X Api running at ${process.env.PORT}...`);
+// });
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, "0.0.0.0", () => {
+	console.log(`X Api running at ${PORT}...`);
 });
--- start.sh
+++ start.sh
@@ -1,5 +1,18 @@
-# Start Nginx
-service nginx start
+# # Start Nginx
+# # service nginx start
+# nginx -g "daemon off;"
+# # nginx -t
+
+# # Start MongoDB
+# mongod --config /etc/mongod.conf &
+
+# # Sleep for a few seconds
+# sleep 3
+
+# # Run your Node.js application
+# nodemon index.js
+
+#!/bin/bash
 
 # Start MongoDB
 mongod --config /etc/mongod.conf &
@@ -7,5 +20,8 @@ mongod --config /etc/mongod.conf &
 # Sleep for a few seconds
 sleep 3
 
-# Run your Node.js application
-nodemon index.js
+# Start Node app
+nodemon index.js &
+
+# Start Nginx in foreground
+nginx -g "daemon off;"
